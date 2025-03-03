ca89a633601d86e9c909ccf165e0ea26e3d33c40
Luke Wilson <thelukaswils@gmail.com>
2025-03-02 21:30:48 -0600

    Update homepage intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
 </script>
 
 <h1>The Homepage of Luke Wilson</h1>
-<p>
-  Welcome to my website! I'm a software engineer and I love to write about
-  technology, philosophy, and the intersection of the two. I hope you enjoy
-  reading my posts.
-</p>
+<p>Welcome to my website! I'm a software developer and I love to write about technology, philosophy, and the intersection of the two. I hope you enjoy reading my posts.</p>
+
+<p>Why do I believe evolution is so important when considering psychology, nature, and the very world as we know it? Because evolution has shaped and molded these very things we take as fact. Evolution is our God that crafted our dizzying belief that nothing else could ever be.</p>
+
+<p>But one could never be more wrong.</p>
+
+<p>Nothing on the Earth today was created without intention. It's all a product of evolution. Everything serves some kind of purpose because of the evolutionary algorithm.</p>
 
 <section>
   <h2>Recent Posts</h2>
